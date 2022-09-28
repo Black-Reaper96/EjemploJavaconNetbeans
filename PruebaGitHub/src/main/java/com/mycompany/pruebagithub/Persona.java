fede107f4487dc81ab834c0bec43d16b4f99373a
am199 <am199@DESKTOP-CRREGV5>
2022-09-28 09:00:56 +0200

    nueva clase Persona

---
+++ PruebaGitHub/src/main/java/com/mycompany/pruebagithub/Persona.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebagithub;
+
+/**
+ *
+ * @author am199
+ */
+public class Persona {
+    
+    private int edad;
+    
+    public Persona(int edad){
+        this.edad=edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
